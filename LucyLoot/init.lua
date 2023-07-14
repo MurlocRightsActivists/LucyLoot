f6505613bf8e37168125f3efbcca464319cc4366
TheTruth <thetruth.pagle@outlook.com>
2023-07-14 14:47:27 -0400

    Updated for TOGC

--- LucyLoot/init.lua
+++ LucyLoot/init.lua
@@ -2,14 +2,13 @@ LucyLoot = LibStub("AceAddon-3.0"):NewAddon("LucyLoot", "AceConsole-3.0", "AceHo
 local AC = LibStub("AceConfig-3.0")
 local ACD = LibStub("AceConfigDialog-3.0")
 
+
 local tonumber = tonumber
 local print = print
 local strsplit = strsplit
 
-local itemsDB = {}
 
 function LucyLoot:OnInitialize()
-
     self.db = LibStub("AceDB-3.0"):New("LucyLootDB", self.defaults, true)
 
     AC:RegisterOptionsTable("LucyLoot_Options", self.options)
@@ -25,51 +24,17 @@ function LucyLoot:OnInitialize()
 end
 
 function LucyLoot:OnEnable()
-
     itemsDB = self.db.global.items
 
-    self:SecureHookScript(GameTooltip, "OnTooltipSetItem")
-    self:SecureHookScript(ShoppingTooltip1, "OnTooltipSetItem")
-    self:SecureHookScript(ShoppingTooltip2, "OnTooltipSetItem")
-    self:SecureHookScript(ItemRefTooltip, "OnTooltipSetItem")
-
+    local tooltips = { GameTooltip, ShoppingTooltip1, ShoppingTooltip2, ItemRefTooltip }
+    for _, tooltip in ipairs(tooltips) do
+        self:SecureHookScript(tooltip, "OnTooltipSetItem")
+        self:SecureHookScript(tooltip, "OnHide", self.OnTooltipHide)
+    end
 end
 
 function LucyLoot:OnDisable()
-
     self:UnhookAll()
-
-end
-
-function LucyLoot:OnTooltipSetItem(tt)
-
-    local _, link = tt:GetItem()
-    if link == nil then return end
-    local _, itemId, _, _, _, _, _, _, _, _, _, _, _, _ = strsplit(":", link)
-    Id = tonumber(itemId)
-    if itemsDB[Id] then
-
-        tt:AddLine(" ")
-
-        tt:AddLine("|cffFF8000Primary:|r " .. "|cffFFFF00" .. itemsDB[Id].primary .. "|r")
-
-        if itemsDB[Id].secondary then
-            tt:AddLine("|cffA335EESecondary:|r " .. "|cffFFFF00" .. itemsDB[Id].secondary .. "|r")
-        end
-        if itemsDB[Id].tertiary then
-            tt:AddLine("|cff0070DDTertiary:|r " .. "|cffFFFF00" .. itemsDB[Id].tertiary .. "|r")
-        end
-        if itemsDB[Id].quaternary then
-            tt:AddLine("|cff1EFF00Quaternary:|r " .. "|cffFFFF00" .. itemsDB[Id].quaternary .. "|r")
-        end
-        if itemsDB[Id].notes then
-            tt:AddLine("|cffDD2222Notes:|r " .. "|cffFFFF00" .. itemsDB[Id].notes .. "|r")
-        end
-
-        tt:AddLine(" ")
-
-    end
-
 end
 
 function LucyLoot:SlashCommand(input, editbox)
@@ -86,4 +51,57 @@ function LucyLoot:SlashCommand(input, editbox)
             ACD:Open("LucyLoot_Options")
         end
     end
+end
+
+local c = {
+    primary = { 1, .5, 0 },         -- Legendary (orange)
+    secondary = { .64, .21, .93 },  -- Epic (purple)
+    tertiary = { 0, .44, .87 },     -- Rare (blue)
+    quaternary = { .12, 1, 0 },     -- Uncommon (green)
+    notes = { 1, 0, 0 },            -- Red
+}
+
+local colorCode = {}
+for k, v in pairs(c) do
+    colorCode[k] = string.format("%02x%02x%02x", v[1] * 255, v[2] * 255, v[3] * 255)
+end
+
+local labelLookup = {
+    primary = "Primary",
+    secondary = "Secondary",
+    tertiary = "Tertiary",
+    quaternary = "Quaternary",
+    notes = "Notes",
+}
+
+function LucyLoot:OnTooltipSetItem(tt)
+    local _, link = tt:GetItem()
+    if not link then return end
+
+    local _, itemId, _, _, _, _, _, _, _, _, _, _, _, _ = strsplit(":", link)
+    local Id = tonumber(itemId)
+
+    if not itemsDB[Id] then return end
+
+    tt:AddLine(" ")
+
+    for k, v in pairs(itemsDB[Id]) do
+        local color = colorCode[k]
+        if color then
+            local label = labelLookup[k]
+            local values = {}
+            for value in string.gmatch(v, "%s*([^,]+)%s*,?") do
+                table.insert(values, value)
+            end
+            tt:AddLine("|cff" .. color:upper() .. label .. ":|r " .. table.concat(values, ", "))
+        end
+    end
+
+    tt:AddLine(" ")
+end
+
+function LucyLoot:OnTooltipHide()
+
+    return
+
 end
