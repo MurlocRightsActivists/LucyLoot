65852b6f70b7fecfbd434a37782c8d7dc4954f90
BarnaldNoodle <thetruth.pagle@gmail.com>
2023-10-24 05:51:11 -0400

    Double checked data

--- LucyLoot/options.lua
+++ LucyLoot/options.lua
@@ -1,7 +1,7 @@
 LucyLoot.defaults = {
     global = {
-        enableDivider = true,
         combatDisable = false,
+        enableDivider = true,
         items = {}
     }
 }
@@ -16,20 +16,21 @@ LucyLoot.options = {
             name = "Tooltips",
             inline = true,
             args = {
-                enableDivider = {
+                combatDisable = {
                     type = "toggle",
                     order = 1,
+                    name = "Disable during combat",
+                    get = function(info) return LucyLoot.db.global.combatDisable end,
+                    set = function(info, value) LucyLoot.db.global.combatDisable = value end,
+                },
+                enableDivider = {
+                    type = "toggle",
+                    order = 2,
                     name = "Enable Divider",
                     get = function(info) return LucyLoot.db.global.enableDivider end,
                     set = function(info, value) LucyLoot.db.global.enableDivider = value end,
                 },
-                combatDisable = {
-                    type = "toggle",
-                    order = 2,
-                    name = "Disable during combat",
-                    get = function(info) return LucyLoot.db.global.combatDisable end,
-                    set = function(info, value) LucyLoot.db.global.combatDisable = value end,
-                } 
+                
             }
         }
     }
