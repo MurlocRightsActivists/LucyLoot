caebfece70755e05728b1d3750c5177ad600fdef
GrumpyTruth <BarnaldNoodle@outlook.com>
2024-11-11 12:15:58 -0500

    Revert to v1.4.2

---
+++ LucyLoot/Raids/ICC25H.lua
@@ -0,0 +1,181 @@
+local items = {
+--------------------------------------------------
+--           Marrowgar
+--------------------------------------------------
+[50604] = { --[[ Band of the Bone Colossus ]] primary = 'Kitty, Ass Rogue, Enh Shaman' },
+[50609] = { --[[ Bone Sentinel's Amulet ]] primary = 'Resto Druid' },
+[50611] = { --[[ Bracers of Dark Reckoning ]] primary = 'BloodDK, Prot Warrior' },
+[50709] = { --[[ Bryntroll, the Bone Arbiter ]] primary = 'PhysicalDPS' },
+[50616] = { --[[ Bulwark of Smouldering Steel ]] primary = 'Holy Paladin, Ele Shaman, Resto Shaman' },
+[50613] = { --[[ Crushing Coldwraith Belt ]] primary = 'Boomkin, Arcane/Fire Mage, Holy Paladin\n              Shadow Priest, Shaman, Aff Lock' },
+[50607] = { --[[ Frostbitten Fur Boots ]] primary = 'Bear, Kitty, Survival/MM Hunter\n              Ret Paladin, Ass/Combat Rogue, Arms Warrior' },
+[50608] = { --[[ Frozen Bonespike ]] primary = 'CasterDPS, Healers' },
+[50606] = { --[[ Gendarme's Cuirass ]] primary = 'UnholyDK', secondary = 'PhysicalDPS' },
+[50615] = { --[[ Handguards of Winter's Respite ]] primary = 'CasterDPS, Healers' },
+[50612] = { --[[ Legguards of Lost Hope ]] primary = 'BloodDK' },
+[50614] = { --[[ Loop of the Endless Labyrinth ]] primary = 'CasterDPS' },
+[50610] = { --[[ Marrowgar's Frigid Eye ]] primary = 'Healers', secondary = 'CasterDPS' },
+[50617] = { --[[ Rusted Bonespike Pauldrons ]] primary = 'Healers', secondary = 'CasterDPS' },
+[50605] = { --[[ Snowserpent Mail Helm ]] primary = 'Enh Shaman, Ret Paladin, Hunters' },
+--------------------------------------------------
+--           Deathwhisper
+--------------------------------------------------
+[50647] = { --[[ Ahn'kahar Onyx Neckguard ]] primary = 'Ret Paladin' },
+[50639] = { --[[ Blood-Soaked Saronite Stompers ]] primary = 'FrostDK, UnholyDK, Fury Warrior' },
+[50640] = { --[[ Broken Ram Skull Helm ]] primary = 'Prot Warrior' },
+[50646] = { --[[ Cultist's Bloodsoaked Spaulders ]] primary = 'PhysicalDPS' },
+[50649] = { --[[ Deathwhisper Chestpiece ]] primary = 'CasterDPS' },
+[50650] = { --[[ Fallen Lord's Handguards ]] primary = 'Holy Paladin' },
+[50641] = { --[[ Heartpierce ]] primary = 'PhysicalDPS' },
+[50642] = { --[[ Juggernaut Band ]] primary = 'Prot Paladin, Prot Warrior' },
+[50645] = { --[[ Leggings of Northern Lights ]] primary = 'Survival Hunter, MM Hunter, Arms Warrior' },
+[50652] = { --[[ Necrophotic Greaves ]] primary = 'Healers', secondary = 'CasterDPS' },
+[50648] = { --[[ Nibelung ]] primary = 'CasterDPS' },
+[50644] = { --[[ Ring of Maddening Whispers ]] primary = 'CasterDPS, Healers' },
+[50643] = { --[[ Shoulders of Mercy Killing ]] primary = 'CasterDPS' },
+[50651] = { --[[ The Lady's Brittle Bracers ]] primary = 'Arcane Mage, Shadow Priest, Aff Lock' },
+[50638] = { --[[ Zod's Repeating Longbow ]] primary = 'MM Hunter, Survival Hunter' },
+--------------------------------------------------
+--           Gunship
+--------------------------------------------------
+[50366] = { --[[ Althor's Abacus ]] primary = 'Resto Druid, Disc Priest, Resto Shaman' },
+[50658] = { --[[ Amulet of the Silent Eulogy ]] primary = 'Ele Shaman, Demo Lock, Enh Shaman' },
+[50660] = { --[[ Boneguard Commander's Pauldrons ]] primary = 'Prot Paladin' },
+[50665] = { --[[ Boots of Unnatural Growth ]] primary = 'CasterDPS' },
+[50661] = { --[[ Corp'rethar Ceremonial Crown ]] primary = 'CasterDPS, Healers' },
+[50349] = { --[[ Corpse Tongue Coin ]] primary = 'Tanks' },
+[50663] = { --[[ Gunship Captain's Mittens ]] primary = 'CasterDPS' },
+[50656] = { --[[ Ikfirus' Sack of Wonder ]] primary = 'Bear, Ass Rogue, Combat Rogue' },
+[50659] = { --[[ Polar Bear Claw Bracers ]] primary = 'FrostDK, Ret Paladin' },
+[50664] = { --[[ Ring of Rapid Ascent ]] primary = 'Boomkin, Fire Mage, Holy Paladin\n              Shadow Priest, Ele Shaman, Resto Shaman\n              Aff Lock' },
+[50655] = { --[[ Scourge Hunter's Vambraces ]] primary = 'Survival Hunter, MM Hunter' },
+[50654] = { --[[ Scourgeborne Waraxe ]] primary = 'Combat Rogue' },
+[50653] = { --[[ Shadowvault Slayer's Cloak ]] primary = 'Ret Paladin, Ass Rogue, Enh Shaman' },
+[50657] = { --[[ Skeleton Lord's Circle ]] primary = 'UnholyDK', secondary = 'PhysicalDPS' },
+[50667] = { --[[ Waistband of Righteous Fury ]] primary = 'Healers, CasterDPS' },
+--------------------------------------------------
+--           Saurfang
+--------------------------------------------------
+[50671] = { --[[ Belt of the Blood Nova ]] primary = 'CasterDPS, Healers' },
+[50672] = { --[[ Bloodvenom Blade ]] primary = 'Prot Paladin', secondary = 'UnholyDK, FrostDK, Combat Rogue', tertiary = 'PhysicalDPS' },
+[50363] = { --[[ Deathbringer's Will ]] primary = 'FrostDK, UnholyDK, Kitty\n              Survival Hunter, MM Hunter, Ass Rogue\n              Combat Rogue, Arms Warrior, Fury Warrior' },
+[50668] = { --[[ Greatcloak of the Turned Champion ]] primary = 'Resto Druid, Demo Lock' },
+[50670] = { --[[ Toskk's Maximized Wristguards ]] primary = 'Bear, Kitty, Ass Rogue\n              Combat Rogue, Arms Warrior, Fury Warrior' },
+--------------------------------------------------
+--           Festergut
+--------------------------------------------------
+[50691] = { --[[ Belt of Broken Bones ]] primary = 'BloodDK, Prot Paladin, Prot Warrior' },
+[50692] = { --[[ Black Bruise ]] primary = 'PhysicalDPS' },
+[50689] = { --[[ Carapace of Forgotten Kings ]] primary = 'PhysicalDPS' },
+[50695] = { --[[ Distant Land ]] primary = 'PhysicalDPS' },
+[50701] = { --[[ Faceplate of the Forgotten ]] primary = 'CasterDPS, Healers' },
+[50690] = { --[[ Fleshrending Gauntlets ]] primary = 'FrostDK, Ret Paladin, UnholyDK' },
+[50697] = { --[[ Gangrenous Leggings ]] primary = 'Combat Rogue' },
+[50700] = { --[[ Holiday's Grace ]] primary = 'Disc Priest' },
+[50698] = { --[[ Horrific Flesh Epaulets ]] primary = 'Ele Shaman' },
+[50696] = { --[[ Leather of Stitched Scourge Parts ]] primary = 'CasterDPS, Healers' },
+[50702] = { --[[ Lingering Illness ]] primary = 'Disc Priest, Demo Lock' },
+[50693] = { --[[ Might of Blight ]] primary = 'FrostDK' },
+[50688] = { --[[ Nerub'ar Stalker's Cord ]] primary = 'Survival Hunter, MM Hunter' },
+[50699] = { --[[ Plague Scientist's Boots ]] primary = 'Boomkin, Arcane Mage, Fire Mage\n              Holy Paladin, Disc Priest, Ele Shaman\n              Resto Shaman, Aff Lock, Demo Lock\n              Resto Druid, Shadow Priest' },
+[50694] = { --[[ Plaguebringer's Stained Pants ]] primary = 'Boomkin, Shadow Priest, Aff Lock', secondary = 'Holy Paladin', notes = 'Holy Paladin craft Leggings of Woven Death first' },
+[50703] = { --[[ Unclean Surgical Gloves ]] primary = 'Resto Shaman' },
+--------------------------------------------------
+--           Rotface
+--------------------------------------------------
+[50675] = { --[[ Aldriana's Gloves of Secrecy ]] primary = 'Kitty, Combat Rogue, Fury Warrior' },
+[50682] = { --[[ Bile-Encrusted Medallion ]] primary = 'Tanks' },
+[50681] = { --[[ Blightborne Warplate ]] primary = 'Tanks' },
+[50687] = { --[[ Bloodsunder's Bracers ]] primary = 'Ele Shaman, Resto Shaman, Enh Shaman' },
+[50684] = { --[[ Corpse-Impaling Spike ]] primary = 'Arcane Mage, Fire Mage, Shadow Priest\n              Aff Lock' },
+[50686] = { --[[ Death Surgeon's Sleeves ]] primary = 'Fire Mage, Disc Priest' },
+[50348] = { --[[ Dislodged Foreign Object ]] primary = 'Boomkin, Arcane Mage, Fire Mage\n              Shadow Priest, Ele Shaman, Aff Lock\n              Demo Loc' },
+[50673] = { --[[ Dual-Bladed Pauldrons ]] primary = 'PhysicalDPS' },
+[50679] = { --[[ Helm of the Elder Moon ]] primary = 'Healers, CasterDPS' },
+[50674] = { --[[ Raging Behemoth's Shoulderplates ]] primary = 'UnholyDK', secondary = 'PhysicalDPS' },
+[50676] = { --[[ Rib Spreader ]] primary = 'PhysicalDPS' },
+[50680] = { --[[ Rot-Resistant Breastplate ]] primary = 'Holy Paladin' },
+[50678] = { --[[ Seal of Many Mouths ]] primary = 'PhysicalDPS' },
+[50685] = { --[[ Trauma ]] primary = 'Healers' },
+[50677] = { --[[ Winding Sheet ]] primary = 'FrostDK, UnholyDK' },
+--------------------------------------------------
+--           Putricide
+--------------------------------------------------
+[50707] = { --[[ Astrylian's Sutured Cinch ]] primary = 'Bear, Kitty, Ret Paladin\n              Ass Rogue, Combat Rogue, Arms Warrior' },
+[50708] = { --[[ Last Word ]] primary = 'PhysicalDPS' },
+[50705] = { --[[ Professor's Bloodied Smock ]] primary = 'Resto Druid' },
+[50704] = { --[[ Rigormortis ]] primary = 'CasterDPS' },
+[50706] = { --[[ Tiny Abomination in a Jar ]] primary = 'Ret Paladin, Ass Rogue' },
+--------------------------------------------------
+--           BloodCouncil
+--------------------------------------------------
+[50721] = { --[[ Crypt Keeper's Bracers ]] primary = 'Holy Paladin' },
+[50603] = { --[[ Cryptmaker ]] primary = 'BloodDK', secondary = 'Fury Warrior', tertiary = 'PhysicalDPS' },
+[50713] = { --[[ Geistlord's Punishment Sack ]] primary = 'PhysicalDPS' },
+[50720] = { --[[ Incarnadine Band of Mending ]] primary = 'Disc Priest,' },
+[50710] = { --[[ Keleseth's Seducer ]] primary = 'PhysicalDPS' },
+[50712] = { --[[ Landsoul's Horned Greathelm ]] primary = 'UnholyDK', secondary = 'PhysicalDPS' },
+[50723] = { --[[ Mail of Crimson Coins ]] primary = 'CasterDPS, Healers' },
+[50718] = { --[[ Royal Crimson Cloak ]] primary = 'Prot Paladin, Prot Warrior, BloodDK' },
+[50717] = { --[[ Sanguine Silk Robes ]] primary = 'Resto Druid, Demo Lock,' },
+[50722] = { --[[ San'layn Ritualist Gloves ]] primary = 'Arcane Mage, Disc Priest' },
+[50719] = { --[[ Shadow Silk Spindle ]] primary = 'Boomkin, Arcane Mage, Fire Mage\n              Disc Priest, Shadow Priest, Aff Lock' },
+[50715] = { --[[ Shoulders of Frost-Tipped Thorns ]] primary = 'CasterDPS, Healers' },
+[50716] = { --[[ Taldaram's Plated Fists ]] primary = 'Tanks' },
+[50711] = { --[[ Treads of the Wasteland ]] primary = 'Enh Shaman,' },
+[50714] = { --[[ Valanar's Other Signet Ring ]] primary = 'CasterDPS' },
+--------------------------------------------------
+--           BloodQueen
+--------------------------------------------------
+[50726] = { --[[ Bauble of True Blood ]] primary = 'Healers' },
+[50724] = { --[[ Blood Queen's Crimson Choker ]] primary = 'Boomkin, Arcane Mage, Fire Mage\n              Holy Paladin, Shadow Priest, Resto Shaman\n              Aff Lock' },
+[50727] = { --[[ Bloodfall ]] primary = 'PhysicalDPS' },
+[50725] = { --[[ Dying Light ]] primary = 'CasterDPS, Healers' },
+[50729] = { --[[ Icecrown Glacial Wall ]] primary = 'Prot Paladin, Prot Warrior' },
+[50728] = { --[[ Lana'thel's Chain of Flagellation ]] primary = 'FrostDK, Arms Warrior, Fury Warriors', notes = 'MUST CHOOSE THIS OR CLAW FROM SINDRAGOSA' },
+--------------------------------------------------
+--           Dreamwalker
+--------------------------------------------------
+[50619] = { --[[ Anub'ar Stalker's Gloves ]] primary = 'Enh Shaman' },
+[50632] = { --[[ Boots of the Funeral March ]] primary = 'CasterDPS, Healers' },
+[50630] = { --[[ Bracers of Eternal Dreaming ]] primary = 'Boomkin, Resto Druid' },
+[50620] = { --[[ Coldwraith Links ]] primary = 'FrostDK, UnholyDK, Fury Warrior' },
+[50622] = { --[[ Devium's Eternally Cold Ring ]] primary = 'BloodDK, Bear, Prot Paladin' },
+[50628] = { --[[ Frostbinder's Shredded Cape ]] primary = 'Boomkin, Arcane Mage, Fire Mage\n              Holy Paladin, Disc Priest, Shadow Priest\n              Ele Shaman, Resto Shaman, Aff Lock' },
+[50618] = { --[[ Frostbrood Sapphire Ring ]] primary = 'Survival Hunter, MM Hunter, Ret Paladin\n              Combat Rogue, Arms Warrior, Fury Warrior' },
+[50625] = { --[[ Grinning Skull Greatboots ]] primary = 'BloodDK, Prot Paladin, Prot Warrior' },
+[50623] = { --[[ Leggings of Dying Candles ]] primary = 'CasterDPS, Healers' },
+[50621] = { --[[ Lungbreaker ]] primary = 'Ass Rogue' },
+[50631] = { --[[ Nightmare Ender ]] primary = 'Disc Priest, Demo Lock' },
+[50627] = { --[[ Noose of Malachite ]] primary = 'Tanks' },
+[50629] = { --[[ Robe of the Waking Nightmare ]] primary = 'Fire Mage' },
+[50624] = { --[[ Scourge Reaver's Legplates ]] primary = 'UnholyDK' },
+[50626] = { --[[ Snowstorm Helm ]] primary = 'Healers' },
+--------------------------------------------------
+--           Sindragosa
+--------------------------------------------------
+[50636] = { --[[ Memory of Malygos ]] primary = 'Resto Druid, Arcane Mage, Demo Lock' },
+[50365] = { --[[ Phylactery of the Nameless Lich ]] primary = 'Boomkin, Fire Mage, Shadow Priest\n              Ele Shaman, Aff Lock, Demo Lock\n              Enh Shaman' },
+[50633] = { --[[ Sindragosa's Cruel Claw ]] primary = 'Kitty, Survival Hunter, MM Hunter\n              Ass Rogue, Combat Rogue, Fury Warriors', notes = 'Fury Warriors MUST CHOOSE this or Lanathels' },
+[50364] = { --[[ Sindragosa's Flawless Fang ]] primary = 'BloodDK, Bear, Prot Paladin\n              Prot Warrior' },
+[50635] = { --[[ Sundial of Eternal Dusk ]] primary = 'Resto Druid, Demo Lock' },
+--------------------------------------------------
+--           LichKing
+--------------------------------------------------
+[50731] = { --[[ Archus, Greatstaff of Antonidas ]] primary = 'CasterDPS, Healers' },
+[50732] = { --[[ Bloodsurge, Kel'Thuzad's Blade of Agony ]] primary = 'Arcane Mage, Fire Mage, Aff Lock\n              Demo Lock' },
+[50733] = { --[[ Fal'inrush, Defender of Quel'thalas ]] primary = 'Survival Hunter, MM Hunter', secondary = 'Ass Rogue, Combat Rogue, Arms Warrior\n                  Fury Warrior' },
+[50730] = { --[[ Glorenzelg, High-Blade of the Silver Hand ]] primary = 'BloodDK, Ret Paladin, Fury Warrior' },
+[50737] = { --[[ Havoc's Call, Blade of Lordaeron Kings ]] primary = 'FrostDK, UnholyDK, Combat Rogue\n              Enh Shaman' },
+[50736] = { --[[ Heaven's Fall, Kryss of a Thousand Lies ]] primary = 'Ass Rogue' },
+[50738] = { --[[ Mithrios, Bronzebeard's Legacy ]] primary = 'Prot Warrior' },
+[50735] = { --[[ Oathbinder, Charge of the Ranger-General ]] primary = 'Bear, Kitty', secondary = 'Survival Hunter, MM Hunter, Arms Warrior' },
+[50734] = { --[[ Royal Scepter of Terenas II ]] primary = 'Boomkin, Resto Druid, Holy Paladin\n              Disc Priest, Shadow Priest, Ele Shaman\n              Resto Shaman, Enh Shaman' },
+[50818] = { --[[ Invinsible's Reins ]] primary = 'TheTruth is out there ...'},
+}
+
+for k, v in pairs(items) do
+    if not LucyLoot.defaults.global.items[k] then
+        LucyLoot.defaults.global.items[k] = items[k]
+    end
+end
--- LucyLoot/init.lua
+++ LucyLoot/init.lua
@@ -12,78 +12,96 @@ local ACD = LibStub("AceConfigDialog-3.0")
 local itemsDB = {}
 
 function LucyLoot:OnInitialize()
-	self.db = LibStub("AceDB-3.0"):New("LucyLootDB", self.defaults, true)
-	AC:RegisterOptionsTable("LucyLoot_Options", self.options)
-	self.optionsFrame = ACD:AddToBlizOptions("LucyLoot_Options", "LucyLoot")
-	local profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
 
-	self:RegisterChatCommand("LL", "SlashCommand")
+    self.db = LibStub("AceDB-3.0"):New("LucyLootDB", self.defaults, true)
+    AC:RegisterOptionsTable("LucyLoot_Options", self.options)
+    self.optionsFrame = ACD:AddToBlizOptions("LucyLoot_Options", "LucyLoot")
+    local profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
+
+    self:RegisterChatCommand("LL", "SlashCommand")
 end
 
 function LucyLoot:OnEnable()
-	itemsDB = self.db.global.items
-	self:SecureHookScript(GameTooltip, "OnTooltipSetItem")
-	self:SecureHookScript(ShoppingTooltip1, "OnTooltipSetItem")
-	self:SecureHookScript(ShoppingTooltip2, "OnTooltipSetItem")
-	self:SecureHookScript(ItemRefTooltip, "OnTooltipSetItem")
+    
+    itemsDB = self.db.global.items
+    self:SecureHookScript(GameTooltip, "OnTooltipSetItem")
+    self:SecureHookScript(ShoppingTooltip1, "OnTooltipSetItem")
+    self:SecureHookScript(ShoppingTooltip2, "OnTooltipSetItem")
+    self:SecureHookScript(ItemRefTooltip, "OnTooltipSetItem")
+
 end
 
 function LucyLoot:OnDisable()
-	self:UnhookAll()
+    self:UnhookAll()
 end
 
 local color = {
-	p = "|cffFF8000Primary:|r ",
-	n = "|cffDD2222Notes:|r ",
-	e = "|r", -- end of color
-	d = "|cffFFFF00", -- default color
+    p = "|cffFF8000Primary:|r ",
+    s = "|cffA335EESecondary:|r ",
+    t = "|cff0070DDTertiary:|r ",
+    q = "|cff1EFF00Quaternary:|r ",
+    n = "|cffDD2222Notes:|r ",
+    e = "|r",                       -- end of color
+    d = "|cffFFFF00"                -- default color
 }
 local divider = "-----------------------------------------------------------------"
 
 function LucyLoot:OnTooltipSetItem(tt)
-	if IsEncounterInProgress() then
-		return
-	end
-	if InCombatLockdown() and self.db.global.combatDisable then
-		return
-	end
 
-	local _, link = tt:GetItem()
-	if link == nil then
-		return
-	end
-	local _, itemId, _, _, _, _, _, _, _, _, _, _, _, _ = strsplit(":", link)
-	Id = tonumber(itemId)
+    if IsEncounterInProgress() then return end
+    if (InCombatLockdown() and self.db.global.combatDisable) then return end
 
-	if itemsDB[Id] then
-		if self.db.global.enableDivider then
-			tt:AddLine(divider)
-		end
+    local _, link = tt:GetItem()
+    if link == nil then return end
+    local _, itemId, _, _, _, _, _, _, _, _, _, _, _, _ = strsplit(":", link)
+    Id = tonumber(itemId)
 
-		tt:AddLine(color.p .. color.d .. itemsDB[Id].primary .. color.e)
+    if itemsDB[Id] then
 
-		if itemsDB[Id].notes then
-			tt:AddLine(color.n .. color.d .. itemsDB[Id].notes .. color.e)
-		end
+        if self.db.global.enableDivider then
+            tt:AddLine(divider)
+        end
 
-		if self.db.global.enableDivider then
-			tt:AddLine(divider)
-		end
-	end
+       tt:AddLine(color.p .. color.d .. itemsDB[Id].primary .. color.e)
+
+       if itemsDB[Id].secondary then
+            tt:AddLine(color.s .. color.d .. itemsDB[Id].secondary .. color.e)
+        end
+
+        if itemsDB[Id].tertiary then
+            tt:AddLine(color.t .. color.d .. itemsDB[Id].tertiary .. color.e)
+        end
+
+        if itemsDB[Id].quaternary then
+            tt:AddLine(color.q .. color.d .. itemsDB[Id].quaternary .. color.e)
+        end
+
+        if itemsDB[Id].notes then
+            tt:AddLine(color.n .. color.d .. itemsDB[Id].notes .. color.e)
+        end
+
+        if self.db.global.enableDivider then
+            tt:AddLine(divider)
+        end
+
+    end
+
 end
 
 function LucyLoot:SlashCommand(input, editbox)
-	if input == "enable" then
-		self:Enable()
-		self:Print("Enabled.")
-	elseif input == "disable" then
-		self:Disable()
-		self:Print("Disabled.")
-	else
-		if ACD.OpenFrames["LucyLoot_Options"] then
-			ACD:Close("LucyLoot_Options")
-		else
-			ACD:Open("LucyLoot_Options")
-		end
-	end
+    if input == "enable" then
+        self:Enable()
+        self:Print("Enabled.")
+    elseif input == "disable" then
+        self:Disable()
+        self:Print("Disabled.")
+    else
+        if ACD.OpenFrames["LucyLoot_Options"] then
+            ACD:Close("LucyLoot_Options")
+        else
+            ACD:Open("LucyLoot_Options")
+        end
+    end
 end
+
+
