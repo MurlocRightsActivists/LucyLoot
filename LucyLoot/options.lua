bcd5f8955a66ba73d6d03d67cedbf0d022ffa3ce
GrumpyTruth <BarnaldNoodle@outlook.com>
2024-11-12 07:40:49 -0500

    Added type check for normal to heroic mappings

--- LucyLoot/options.lua
+++ LucyLoot/options.lua
@@ -1,37 +1,45 @@
+---@diagnostic disable
 LucyLoot.defaults = {
-    global = {
-        combatDisable = false,
-        enableDivider = true,
-        items = {}
-    }
+	global = {
+		combatDisable = false,
+		enableDivider = true,
+		items = {},
+	},
 }
 
 LucyLoot.options = {
-    type = "group",
-    name = "LucyLoot Options",
-    handler = LucyLoot,
-    args = {
-        group1 = {
-            type = "group",
-            name = "Tooltips",
-            inline = true,
-            args = {
-                combatDisable = {
-                    type = "toggle",
-                    order = 1,
-                    name = "Disable during combat",
-                    get = function(info) return LucyLoot.db.global.combatDisable end,
-                    set = function(info, value) LucyLoot.db.global.combatDisable = value end,
-                },
-                enableDivider = {
-                    type = "toggle",
-                    order = 2,
-                    name = "Enable Divider",
-                    get = function(info) return LucyLoot.db.global.enableDivider end,
-                    set = function(info, value) LucyLoot.db.global.enableDivider = value end,
-                },
-                
-            }
-        }
-    }
+	type = "group",
+	name = "LucyLoot Options",
+	handler = LucyLoot,
+	args = {
+		group1 = {
+			type = "group",
+			name = "Tooltips",
+			inline = true,
+			args = {
+				combatDisable = {
+					type = "toggle",
+					order = 1,
+					name = "Disable during combat",
+					get = function(info)
+						return LucyLoot.db.global.combatDisable
+					end,
+					set = function(info, value)
+						LucyLoot.db.global.combatDisable = value
+					end,
+				},
+				enableDivider = {
+					type = "toggle",
+					order = 2,
+					name = "Enable Divider",
+					get = function(info)
+						return LucyLoot.db.global.enableDivider
+					end,
+					set = function(info, value)
+						LucyLoot.db.global.enableDivider = value
+					end,
+				},
+			},
+		},
+	},
 }
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # LucyLoot
-##### Classic Wrath AddOn for Murloc Rights Activists of Pagle.
-##### This will add the guilds roll priority to item tooltips.
+
+##### Classic Cataclysm AddOn for Murloc Rights Activists of Pagle
+
+##### This will add the guilds roll priority to item tooltips
+
 ![](https://github.com/MurlocRightsActivists/LucyLoot/blob/ed2418c778ea3795dad96bdd3286871285415be0/preview.png)
